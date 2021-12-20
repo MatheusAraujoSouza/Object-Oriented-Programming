612faa10b3e4a074cbdb99331f1b1499e291a4e0
Matheus Araujo Souza <63359384+MatheusAraujoSouza@users.noreply.github.com>
2021-12-20 10:55:53 -0300

    Add files via upload

---
+++ getters and setters/Aluno.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Aluno
+{
+    private string nome;
+
+    //Get and Set
+    public string Nome
+    {
+        get{return nome;}
+        set{nome = value ;}
+    }
+
+}
---
+++ getters and setters/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando
+            Aluno novo = new Aluno();
+            novo.Nome = "Matheus araujo Souza"; //Agora vou usar o setter
+            Console.WriteLine(novo.Nome);// Assim estou usando um getter
+        }
+    }
+
+}
