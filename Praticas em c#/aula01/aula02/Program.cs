b6073b6089a214ab225a088e8cb8e0377a9e1535
Matheus Araujo Souza <63359384+MatheusAraujoSouza@users.noreply.github.com>
2021-12-23 20:56:32 -0300

    Add files via upload

---
+++ Praticas em c#/aula01/aula02/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace aula02
+{
+    class Program
+    {
+        enum Dias_da_semana{Segunda, Terça, Quarta, Quinta,Sexta,Sábado,Domingo};
+        static void Main()
+        {
+            //Lendo informações do usuario
+            int v1,v2;
+            Console.Write("Digite o valor da primeira variavel: ");
+            v1= Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Digite o valor da primeira segunda: ");
+            v2= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("O valor do produto das variaveis é :{0}",(v1*v2));
+
+
+            //Operação de BitWise 
+            int num1 = 2,num2=2;
+            //Ao realizar o bitwise vai ocorrer umd eslocamento a a esquerda e o valor vazio vai para ser zero
+            num1 = num1 <<1;//o que era 00000010 vai para 0000100
+            Console.WriteLine(num1);
+            num2 = num2 << 2;//o bitwise vai fazer com que o valor dobre duas vezes 00000010 vai para 00001000
+            Console.WriteLine(num2);
+            num2 = num2 >> 2; // lo bitwise vai fazer com o que o valor diminua pela metade duas vezes
+            Console.WriteLine(num2);
+            
+            //Enum, com o enum podemos referenciar os valores que o conjunto pode assumir 
+            //com os index dele
+            
+            Dias_da_semana p = Dias_da_semana.Segunda;
+            Console.WriteLine(p);
+            //Tambem é possivel passar o index
+            p = (Dias_da_semana)2;
+            Console.WriteLine(p);
+            //também é possivel transformar o valor em index
+            int index = (int)Dias_da_semana.Segunda;
+            Console.WriteLine(index);
+
+
+
+
+        }
+    }
+}
---
+++ Praticas em c#/aula01/aula03/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace aula03
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Lendo o numero do usuario
+            Console.Write("Digite um valor inteiro n : ");
+            int n = int.Parse(Console.ReadLine());
+        
+            //IF/ELSE IF/ElSE 
+            
+
+            if(n < 45){
+                Console.WriteLine("Reprovado por ser < 45");
+            }
+            else if (n < 49)
+            {
+                Console.WriteLine("Reprovado por ser < 49");
+            }
+            else if(n < 60)
+            {
+                 Console.WriteLine("Exame por ser < 60");
+            }else
+            {
+                Console.WriteLine("Aprovado");
+            }
+
+            //Segundo teste de ifs alinhados
+
+            if(n >=60){
+                if(n>=90){
+                    Console.WriteLine("Aprovado com meritos");
+                }else{
+                    Console.WriteLine("Apenas aprovado");
+                }
+            }
+            else{
+                if(n < 45){
+                    Console.WriteLine("Reprovado por nota");
+                }else{
+                    Console.WriteLine("Exame");
+                }
+            }
+
+            inicio:
+      
+            //SWITCH
+            
+            Console.WriteLine("Digite um caractere para indicar o meio de transporte");
+            Console.WriteLine("Onibus[o], Carro[c], Bicicleta[b], Avião[a] ");
+            char character = Convert.ToChar(Console.ReadLine());
+            switch(character){
+                case 'a':
+                case 'A':
+                    Console.WriteLine("O preço da passagem de aviao para seu destino é R$: 4000");
+                    break;
+                case 'o':
+                case 'O':
+                    Console.WriteLine("O preço da passagem de onibus para seu destino é R$: 500");
+                    break;
+                case 'c':
+                case 'C':
+                    Console.WriteLine("O preço da passagem de carro para seu destino é R$: 470");
+                    break;
+                case 'b':
+                case 'B':
+                    Console.WriteLine("Boa sorte!!");
+                    break;
+                default:
+                    Console.WriteLine("letra digitada incorreta");
+                    break;
+
+            }
+            Console.Write("Deseja consultar outro meio de transporte s < > n? :");
+            char resp = Convert.ToChar(Console.ReadLine());
+            if(resp == '<'){
+                Console.Clear();
+                goto inicio;
+            }
+            //goto inicio;
+
+
+        }
+    }
+}
