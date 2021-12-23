91a65faf6a8573d6eecc41bff4a0473f90ba6365
Matheus Araujo Souza <63359384+MatheusAraujoSouza@users.noreply.github.com>
2021-12-23 20:58:31 -0300

    Add files via upload

---
+++ Praticas em c#/aula01/aula04/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace aula04{
+    class Program{
+        static void Main(){
+            //Tipos de declaração de arrays 
+            int[] n = new int[4];
+            int[] num = {40,12,339};
+            int[] n2 = new int[5]{0,0,0,0,0};
+            Console.WriteLine(num[2]);
+            Console.WriteLine(n2[4]);
+
+            //Arrays bidimencionais
+            int[,] n4 = new int[2,2]{{2,5},{6,7}};//Primeira maneira de declaracao
+            int[,] n5 = new int[2,3];
+            n5[1,1] = 0;
+            Console.WriteLine(n4[0,0]);
+            Console.WriteLine(n5[1,1]);
+
+            //Usando loop para printar o vetor e logo depois a matrix 
+            for(int i=0;i<3;i++){//printando o vetor num
+                Console.Write(num[i]+" ");
+            }
+            Console.Write("\n");
+            
+            for(int i=0;i<5;i++){//printando o vetor n5
+                Console.Write(n2[i]+" ");
+            }
+            Console.Write("\n");
+            
+            for(int i=0;i<2;i++){//printando a matriz n4
+                for(int j=0;j<2;j++){
+                    Console.Write(n4[i,j]+" ");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+
+            //Usando a forma de While que executa enquanto a condicao ser verdadeira
+            int jj=0;
+            while(jj<3){
+                Console.Write(num[jj]+" ");
+                jj++;
+            }
+            Console.Write("\n");
+
+            //DO - While
+            //Praticando co uma senha 
+            string senha = "10203040Deus";
+            string senha_User;
+            do{
+                //Console.Clear();
+                Console.Write("Digite a senha de segurança: ");
+                senha_User = Console.ReadLine();
+                
+                if(senha_User != senha){
+                    Console.Write("Senha incorreta");
+                    Console.ReadLine();
+                    Console.Clear();
+                }else{
+                    break;
+                }
+
+            }while(true);
+
+
+
+            //FOREACH 
+            int contador=0;
+            foreach(int i in n4){//Lendo uma matriz com um único loop
+                Console.Write(i+" ");
+                contador++;
+                if(contador == 2){
+                    Console.Write("\n");
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ Praticas em c#/aula01/aula05/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace aula05{
+    class Program{
+        static void Main(){
+
+
+            //Métodos para array
+
+            //Declarando os arrays 
+            int [] vet1 = new int[100];
+            int [] vet2 = new int[100];
+            int [] vet3 = new int[100];
+            int [,] matriz = new int[2,5]{{12,54,54,65,23},{456,65,23,556,98}};
+            int[] vet_test = {1,2,3,4,5,6,7,8,9,10};
+
+            //Gerando numeros randomicos
+            /*
+            The Next() Method of System.
+            Random class in C# is used to get a random integer number. 
+            This method can be overloaded by passing different parameters
+            */
+            Random rand = new Random();
+            for(int i=0;i<100;i++){
+                vet1[i]=rand.Next(0,100);
+            }
+            Console.WriteLine("Elementos do vetor 1");
+            foreach(int i in vet1){
+                Console.Write(i+" ");
+            }
+            Console.Write("\n");
+
+            Array.Sort(vet1);
+
+            Console.WriteLine("Print do vetor ordenado");
+            foreach(int i in vet1){
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+            //public static int BinarySearch(arry, valor)
+            Console.WriteLine("BinarySearch");
+            int valor_procurado = 3;
+            int pos = Array.BinarySearch(vet1,valor_procurado);
+            Console.WriteLine("A posicao do elemento {0} é : {1} ",valor_procurado,pos);
+
+            pos = Array.BinarySearch(vet_test,valor_procurado);
+            Console.WriteLine("A posicao do elemento {0} é : {1} ",valor_procurado,pos);
+
+            //public static void Copy(Array origem, array destino, quantidade de elementos)
+            Console.WriteLine("Função Copy");
+            Array.Copy(vet1,vet2,vet1.Length);
+
+            foreach(int i in vet2){
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+
+            //public void CopyTo(Array destino, a partir dessa posicao)
+            Console.WriteLine("CopoyTo");
+            vet2.CopyTo(vet3,0);
+            foreach(int i in vet3){
+                Console.Write(i+" ");
+            }
+            Console.Write("\n");
+
+            //public long Getlonglength(Dimensão)
+            Console.WriteLine("Getlonglength");
+            long quantidade_de_elementos_no_vetor=vet1.GetLongLength(0);
+            Console.WriteLine("A quantidade de elementos no vetor é {0}",quantidade_de_elementos_no_vetor);
+
+
+
+
+
+            //public int GetlowerBound(Dimensão)
+            Console.WriteLine("GetlowerBound");
+            int Menor_indice_do_vetor=vet1.GetLowerBound(0);
+            int Menor_indice_da_matriz=matriz.GetLowerBound(1);
+            Console.WriteLine("A quantidade de elementos no vetor : {0} e a \n quantidade de elementos na matriz: {1}",Menor_indice_do_vetor,Menor_indice_da_matriz);
+
+
+            //public int GetUpperBound(Dimensão)
+            Console.WriteLine("GetUpperBound");
+            int Maior_indice_do_vetor = vet1.GetUpperBound(0);
+            int Maior_indice_da_matriz = matriz.GetUpperBound(1);
+            Console.WriteLine("O maior indice do vetor: {0} e o menor indice da matriz {1}",Maior_indice_do_vetor,Maior_indice_da_matriz);
+
+            //get Value, basicamente ele vai me retornar o valor está na posicao do indice dado 
+            Console.WriteLine("Getvalue");
+            int valor0 = Convert.ToInt32(vet1.GetValue(3));
+            int valor1= Convert.ToInt32(matriz.GetValue(1,2));
+            Console.WriteLine("Os valores no index 3 : {0} e o valor no index [1,2]: {1}",valor0,valor1);
+
+            int[] vet_testt = {1,2,3,4,6,7,8,3};
+            //public static indexoF(array, valor)
+            Console.WriteLine("indexpf");
+            int indece1 = Array.IndexOf(vet_testt,3);
+            Console.WriteLine("Indece do primeiro valor 3 : {0}", indece1);
+
+            
+            //public static indexlast
+            Console.WriteLine("idenxlast");
+            int indece2 = Array.LastIndexOf(vet_testt,3);
+            Console.WriteLine("O ultimo elemento que paarece o index: {0}",indece2);
+
+
+            //public static void Reverse(array)
+            Array.Reverse(vet1);
+            foreach(int i in vet1){
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+
+            //Metodo Set value 
+            int[] vetor44 = {23,5,6,7,8};
+            vetor44.SetValue(1900,0);
+            foreach(int i in vetor44){
+                Console.Write(i+" ");
+            }
+            Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
